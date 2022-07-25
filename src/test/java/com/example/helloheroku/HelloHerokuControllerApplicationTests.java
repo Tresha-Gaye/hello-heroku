efb2425724adee4fe79a54aae42645a96e3f2b61
Twigster <treshagaye@gmail.com>
2022-07-25 15:13:05 -0400

    initial commit

---
+++ src/test/java/com/example/helloheroku/HelloHerokuControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHerokuControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
